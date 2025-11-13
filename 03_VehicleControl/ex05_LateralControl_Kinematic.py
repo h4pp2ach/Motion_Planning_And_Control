468abae68fec59ef2e3e6c035338c7c752ccb2cf
h4pp2ach <jin4god@inu.ac.kr>
2025-11-13 09:12:53 +0900

    Lateral Control with PID

--- 03_VehicleControl/ex05_LateralControl_Kinematic.py
+++ 03_VehicleControl/ex05_LateralControl_Kinematic.py
@@ -4,10 +4,28 @@ import matplotlib.pyplot as plt
 from VehicleModel_Lat import VehicleModel_Lat
 
 class PID_Controller_Kinematic(object):
-    def __init__(self):
-        # Code
-    def ControllerInput(self):
-        # Code
+    def __init__(self, dt, Y_ref, ego_Y, Kp, Kd, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.dt = dt
+        
+        self.error = Y_ref - ego_Y
+        self.error_prev = self.error
+        self.error_sum = 0
+        
+        self.u = 0
+        
+    def ControllerInput(self, Y_ref, ego_Y):
+        self.error = Y_ref - ego_Y
+        self.error_sum += self.error * self.dt
+        
+        P_term = self.Kp * (self.error)
+        I_term = self.Ki * (self.error_sum)
+        D_term = self.Kd * (self.error - self.error_prev) / self.dt
+        
+        self.u = P_term + I_term + D_term
+        self.error_prev = self.error
 
     
 if __name__ == "__main__":
@@ -20,7 +38,9 @@ if __name__ == "__main__":
     X_ego = []
     Y_ego = []
     ego_vehicle = VehicleModel_Lat(step_time, Vx)
-    controller = PID_Controller_Kinematic(step_time, Y_ref, ego_vehicle.Y)
+    controller = PID_Controller_Kinematic(step_time, Y_ref, ego_vehicle.Y,
+                                          Kp = 0.1, Kd = 0.4, Ki = 0.0)
+    
     for i in range(int(simulation_time/step_time)):
         time.append(step_time*i)
         X_ego.append(ego_vehicle.X)
